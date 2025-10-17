fe33c28c0f980cb23f8f06bf415ce12fcacb89f0
Jose <pepeisa.ortiz@gmail.com>
2025-10-17 14:26:17 -0500

    del test Chat.jsx

--- src/Chat.jsx
+++
@@ -1,9 +0,0 @@
-function Chat() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-2xl text-gray-800">Hola</h1>
-    </div>
-  );
-}
-
-export default Chat;
